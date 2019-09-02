c27da4f8f00719e15b72c75f5ab486a0e2a37edf
Sam Oakley <samwoakley@gmail.com>
2019-09-02 23:36:31 +0100

    Actually got the server side working

--- prices/price_generator.py
+++ prices/price_generator.py
@@ -1,21 +1,25 @@
 import numpy as np
 import websockets
 import asyncio
-import datetime
-
-async def send_prices(websocket, path):
-    current_time = datetime.datetime.now()
-    price = get_next_price(previous_price, current_time - previous_time) + DRIFT
-    print(price)
-    await websocket.send(price)
-
-def get_next_price(p, delta_t):
-    return p + delta_t**2 * np.random.normal(0, 1)
+from datetime import datetime, timedelta
 
 START_PRICE = 0
 previous_price = START_PRICE
-previous_time = datetime.datetime.now()
+previous_time = datetime.now()
 DRIFT = 0
+
+async def send_prices(websocket, path):
+    global previous_price, previous_time
+    current_time = datetime.now()
+    delta_t = (current_time - previous_time).total_seconds()
+    previous_time = current_time
+    price = get_next_price(previous_price, delta_t) + DRIFT
+    previous_price = price
+    print(price)
+    await websocket.send(str(price))
+
+def get_next_price(p, delta_t):
+    return p + delta_t**2 * np.random.normal(0, 1)
 
 start_server = websockets.serve(send_prices, "localhost", 8765)
 asyncio.get_event_loop().run_until_complete(start_server)
