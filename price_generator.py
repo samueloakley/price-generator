fixed calculation for drift

price_generator.py
@@ -4,9 +4,9 @@ import asyncio
 from datetime import datetime, timedelta
 import json
 
-START_PRICE = 0
+START_PRICE = 10
 previous_price = START_PRICE
-DRIFT = 0
+DRIFT = 0.5
 
 async def send_prices(websocket, path):
     global previous_price, previous_time
@@ -15,14 +15,14 @@ async def send_prices(websocket, path):
         current_time = datetime.now()
         delta_t = (current_time - previous_time).total_seconds()
         previous_time = current_time
-        price = get_next_price(previous_price, delta_t) + DRIFT
+        price = get_next_price(previous_price, delta_t) + DRIFT * delta_t
         previous_price = price
         print(price)
 
         message = {'time': str(current_time.timestamp()), 'price': price}
 
-        await websocket.send(json.dumps(message))
         await asyncio.sleep(1)
+        await websocket.send(json.dumps(message))
 
 def get_next_price(p, delta_t):
     return p + delta_t**2 * np.random.normal(0, 1)